7fe22ca0f83db980933e8b9627fba9bdfa0f0dbe
Muhamad Safei <msafei.ti@gmail.com>
2025-02-16 10:55:58 +0700

    add-userController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('add-user');
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'id' => Str::uuid(),
+            'username' => $request->username,
+            'password' => $request->password,
+        ]);
+
+        return redirect()->route('dashboard')->with('success', 'User berhasil ditambahkan');
+    }
+}
