57902f69e80d446b257f0f527a05ac9ab7e51a3b
Muhamad Safei <msafei.ti@gmail.com>
2025-02-16 10:53:51 +0700

    add-routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Redirect root ke login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+// Route Login
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'processLogin'])->name('login.process');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Dashboard (Hanya bisa diakses jika login)
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/add-user', [UserController::class, 'create'])->name('add-user');
+    Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
 });
